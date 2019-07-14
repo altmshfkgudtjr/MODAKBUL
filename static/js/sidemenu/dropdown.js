d5feb74434ea8fcf86fab6d6b2df4269b6313f2f
NB777 <altmshfkgudtjr@naver.com>
2019-07-14 18:57:22 +0900

    2019.07.14 ver.1

---
+++ static/js/sidemenu/dropdown.js
@@ -0,0 +1,15 @@
+function M_dropdown_money() {
+	$("#M_dropdown_icon_money").toggleClass("fa-sort-down");
+	$("#M_dropdown_icon_money").toggleClass("fa-sort-up M_dropdwon_trf");
+	$("#M_studentMoney").animate({height: 'toggle'});
+}
+function M_dropdown_competition() {
+	$("#M_dropdown_icon_competition").toggleClass("fa-sort-down");
+	$("#M_dropdown_icon_competition").toggleClass("fa-sort-up M_dropdwon_trf");
+	$("#M_studentCompetition").animate({height: 'toggle'});
+}
+function M_dropdown_volunteer() {
+	$("#M_dropdown_icon_volunteer").toggleClass("fa-sort-down");
+	$("#M_dropdown_icon_volunteer").toggleClass("fa-sort-up M_dropdwon_trf");
+	$("#M_studentVolunteer").animate({height: 'toggle'});
+}
