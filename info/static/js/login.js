163b45c21d0e9d6d6866a17b57c425eaab04dd72
NB777 <altmshfkgudtjr@naver.com>
2019-07-16 19:11:59 +0900

    2019.07.16 ver.4

--- info/static/js/login.js
+++ info/static/js/login.js
@@ -1,33 +1,62 @@
 //로그인 버튼을 누를 시, 실행되는 함수
-function user_login()
-{
+function user_login(){
 	var login_ID = $('#user_id').val();	// user_id 란 ID 값의 value 값 가져옴
 	var login_PW = $('#user_pw').val();	// user_pw 란 ID 값의 value 값 가져옴
 	
 	if (login_ID.length <= 0 || login_PW.length <= 0){
-		alert("아이디 및 비밀번호를 입력해주세요.");
+		snackbar("아이디 및 비밀번호를 입력해주세요.");
 		return;
 	}
 
 	var send_data = {id: login_ID, pw: login_PW};
-	var a_jax = A_JAX(TEST_IP+"sign_in_up", "POST", null, send_data);	//"/login" 이라는 url에 아이디/비밀번호 data 전송
+	var a_jax = A_JAX(TEST_IP+"sign-in-up", "POST", null, send_data);	//"/login" 이라는 url에 아이디/비밀번호 data 전송
 	$.when(a_jax).done(function(){
 		console.log(a_jax);
 		var json = a_jax.responseJSON;
-		if (json['result'] == "your not Sejong"){			// result 값이 "your not Sejong" 이라면 실행
-			alert("로그인에 실패하였습니다.");
+		if (json['result'] == "you are not sejong"){			// result 값이 "your not Sejong" 이라면 실행
+			snackbar("올바르지 않은 회원 정보입니다.");
 		}
-		else if (json['result'] == "password incorrect"){	// result 값이 "password incorrect" 이라면 실행
-			alert("비밀번호를 다시 입력해주세요.");
+		else if (json['result'] == "wrong info"){	// result 값이 "password incorrect" 이라면 실행
+			snackbar("비밀번호를 다시 입력해주세요!");
 		}
 		else if (json['result'] == "success"){				// result 값이 "success" 이라면 실행
 			// 로그인 성공 token 생성
 			localStorage.setItem('modakbul_token', json['access_token']);
-			alert("로그인 성공");
-			/* 로그인 성공 시, 취할 액션 집어넣기 */
+			snackbar("로그인 성공!");
+			location.href = "/";
 		}
 		else {
-			alert("일시적인 오류가 발생하였습니다. 잠시 후 다시 시도해주세요.");
+			snackbar("올바르지 않은 회원 정보입니다.");
 		}
 	});
 }
+//로그아웃 버튼을 누를 시, 실행되는 함수
+function user_logout(){
+	localStorage.removeItem('modakbul_token');
+	snackbar("로그아웃 되었습니다.");
+	$("#M_nav_user_login").animate({height: 'hide'}, 'fast');
+}
+
+//엔터 로그인
+function login_enter(){
+    if (window.event.keyCode == 13) {
+        user_login();
+    }
+}
+
+//로그인 창 애니메이션 함수
+(function ($) {
+    "use strict";
+    /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function(){
+        $(this).on('blur', function(){
+            if($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })    
+    })
+})(jQuery);
--- src/static/js/themeChange.js
+++ src/static/js/themeChange.js
@@ -1,46 +1,64 @@
+function darkSetTheme() {
+	$('.M_theme').removeClass("fa-moon");
+	$('.M_theme').addClass("fa-sun");
+	$('body').addClass('M_dark_theme M_dark_theme_body');
+	$('#M_body').addClass('M_dark_theme M_dark_theme_body');
+	$('#gn-menu').addClass('M_dark_theme M_boxshadow_dark_shadow');
+	$('#M_fire_logo').addClass('M_dark_theme');
+	$('#gn-menu-wrapper').addClass('M_boxshadow_dark_sidemenu');
+	$('#M_nav_user_login').addClass('M_boxshadow_dark_user M_dark_theme');
+	$('#M_nav_user_nologin').addClass('M_boxshadow_dark_user M_dark_theme');
+	$('#gn-scroller').addClass('M_dark_theme');
+	$('.gn-menu-main ul').css({"background-color": "#202124", "color": "#e2e2e2"});
+	$('#M_gn-icon').addClass('M_dark_theme');
+	$('.M_info_div').css({"background-color": "#494e52", "color": "#f5f6fa"});
+	$('.gn-menu-wrapper').css("background-color", "#202124");
+	$('meta[name="theme-color"]').attr('content', "#202124");
+	$('.input100').css("color", "#f5f6fa");
+	$('#M_nav_user_nologin a').css("color", "#e2e2e2");
+}
+function whiteSetTheme() {
+	$('.M_theme').removeClass("fa-sun");
+	$('.M_theme').addClass("fa-moon");
+	$('body').removeClass('M_dark_theme M_dark_theme_body');
+	$('#M_body').removeClass('M_dark_theme M_dark_theme_body');
+	$('#gn-menu').removeClass('M_dark_theme M_boxshadow_dark_shadow');
+	$('#M_fire_logo').removeClass('M_dark_theme');
+	$('#gn-menu-wrapper').removeClass('M_boxshadow_dark_sidemenu');
+	$('#M_nav_user_login').removeClass('M_boxshadow_dark_user M_dark_theme');
+	$('#M_nav_user_nologin').removeClass('M_boxshadow_dark_user M_dark_theme');
+	$('#gn-scroller').removeClass('M_dark_theme');
+	$('.gn-menu-main ul').css({"background-color": "white", "color": '#5f6f81'});
+	$('#M_gn-icon').removeClass('M_dark_theme');
+	$('.M_info_div').css({"background-color": "white", "color": "#3E5569"});
+	$('.gn-menu-wrapper').css("background-color", "white");
+	$('meta[name="theme-color"]').attr('content', "white");
+	$('.input100').css("color", "#555555");
+	$('#M_nav_user_nologin a').css("color", "#5f6f81");
+}
 function changeTheme() {
-	var now_theme = $('#M_theme').attr('class');
-	// now thema : white
-	if (now_theme.indexOf("moon") != -1 ){
-		$('.M_theme').removeClass("fa-moon");
-		$('.M_theme').addClass("fa-sun");
-		$('body').addClass('M_dark_theme M_dark_theme_body');
-		$('#M_body').addClass('M_dark_theme M_dark_theme_body');
-		$('#gn-menu').addClass('M_dark_theme M_boxshadow_dark_shadow');
-		$('#M_fire_logo').addClass('M_dark_theme');
-		$('#gn-menu-wrapper').addClass('M_boxshadow_dark_sidemenu');
-		$('#M_nav_user_login').addClass('M_boxshadow_dark_user M_dark_theme');
-		$('#M_nav_user_nologin').addClass('M_boxshadow_dark_user M_dark_theme');
-		$('#gn-scroller').addClass('M_dark_theme');
-		$('.gn-menu-main ul').css({"background-color": "#202124", "color": "#e2e2e2"});
-		$('#M_gn-icon').addClass('M_dark_theme');
-		$('.M_info_div').css({"background-color": "#494e52", "color": "#f5f6fa"});
-		$('.gn-menu-wrapper').css("background-color", "#202124");
-		$('meta[name="theme-color"]').attr('content', "#202124");
-		$('.input100').css("color", "#f5f6fa");
-		$('#M_nav_user_nologin a').css("color", "#e2e2e2");
+	// now_theme check
+	if (localStorage.getItem('modakbul_theme') == null){
+		localStorage.setItem('modakbul_theme', "dark");
+	} else if (localStorage.getItem('modakbul_theme') == 'white') {
+		localStorage.setItem('modakbul_theme', "dark");
+	} else {
+		localStorage.setItem('modakbul_theme', "white");
 	}
-	// now thema : dark
+	var now_theme = localStorage.getItem('modakbul_theme');
+	if (now_theme == 'white'){
+		whiteSetTheme();
+	}
 	else {
-		$('.M_theme').removeClass("fa-sun");
-		$('.M_theme').addClass("fa-moon");
-		$('body').removeClass('M_dark_theme M_dark_theme_body');
-		$('#M_body').removeClass('M_dark_theme M_dark_theme_body');
-		$('#gn-menu').removeClass('M_dark_theme M_boxshadow_dark_shadow');
-		$('#M_fire_logo').removeClass('M_dark_theme');
-		$('#gn-menu-wrapper').removeClass('M_boxshadow_dark_sidemenu');
-		$('#M_nav_user_login').removeClass('M_boxshadow_dark_user M_dark_theme');
-		$('#M_nav_user_nologin').removeClass('M_boxshadow_dark_user M_dark_theme');
-		$('#gn-scroller').removeClass('M_dark_theme');
-		$('.gn-menu-main ul').css({"background-color": "white", "color": '#5f6f81'});
-		$('#M_gn-icon').removeClass('M_dark_theme');
-		$('.M_info_div').css({"background-color": "white", "color": "#3E5569"});
-		$('.gn-menu-wrapper').css("background-color", "white");
-		$('meta[name="theme-color"]').attr('content', "white");
-		$('.input100').css("color", "#555555");
-		$('#M_nav_user_nologin a').css("color", "#5f6f81");
+		darkSetTheme();
 	}
 }
-//#323639
-//#202124
-//e2e2e2
+function setTheme() {
+	if (localStorage.getItem('modakbul_theme') == null){
+		localStorage.setItem('modakbul_theme', "white");
+	} else if (localStorage.getItem('modakbul_theme') == 'white') {
+		whiteSetTheme();
+	} else {
+		darkSetTheme();
+	}
+}
