a03947765312025e1c4f6068d7d8962e6a15eb75
NB777 <altmshfkgudtjr@naver.com>
2019-07-26 05:21:36 +0900

    post modal really done, main page done, media query done

--- src/static/js/infinitescroll.js
+++ src/static/js/infinitescroll.js
@@ -7,133 +7,132 @@ function format_time(raw_data) {
 
     return year+'년 '+month+'월 '+date+'일 '+time;
 }
-
+let flag = true;
 $(window).ready(function() {
     let a_jax = A_JAX(TEST_IP+'get_posts/공지/1', 'GET', null, null);
         $.when(a_jax).done(function () {
-            let div_class = 'M_info_div M_board_content M_boxshadow wow flipInX';
-            if (localStorage.getItem('modakbul_theme') === 'dark') {
-                div_class +=  ' M_boxshadow_dark_shadow" style="' +
-                    'visibility: visible; background-color: rgb(73, 78, 82); color: rgb(245, 246, 250); border: 0px solid rgb(221, 221, 221);';
-            }
-            for (let i=0; i<a_jax.responseJSON.posts.length; i++) {
-                let data = a_jax.responseJSON.posts[i];
-                let submission_time = data.post_date;
-                let title = data.post_title;
-                let views = data.post_view;
-                let comment_count = data.comment_cnt;
-                let likes = data.like_cnt;
-                let post_id = data.post_id;
-                let user_color = data.author_color;
-                let is_image_icon = data.img_cnt;
-                let is_attachment_icon = data.file_cnt;
-                let is_lock_icon = data.private;
-                if (is_image_icon == 0){
-                    is_image_icon = "M_board_content_image_none";
-                } else {
-                    is_image_icon = "M_board_content_image_block";
+            if (a_jax.responseJSON['result'] == 'success'){
+                flag = false;
+                let div_class = 'M_info_div M_board_content M_boxshadow wow flipInX';
+                if (localStorage.getItem('modakbul_theme') === 'dark') {
+                    div_class +=  ' M_boxshadow_dark_shadow" style="' +
+                        'visibility: visible; background-color: rgb(73, 78, 82); color: rgb(245, 246, 250); border: 0px solid rgb(221, 221, 221);';
                 }
-                if (is_attachment_icon == 0) {
-                    is_attachment_icon = "M_board_content_paperclip_none";
-                } else {
-                    is_attachment_icon = "M_board_content_paperclip_block";
+                for (let i=0; i<a_jax.responseJSON.posts.length; i++) {
+                    let data = a_jax.responseJSON.posts[i];
+                    let submission_time = data.post_date;
+                    let title = data.post_title;
+                    let views = data.post_view;
+                    let comment_count = data.comment_cnt;
+                    let likes = data.like_cnt;
+                    let post_id = data.post_id;
+                    let user_color = data.author_color;
+                    let is_image_icon = data.img_cnt;
+                    let is_attachment_icon = data.file_cnt;
+                    let is_lock_icon = data.private;
+                    if (is_image_icon == 0){
+                        is_image_icon = "M_board_content_image_none";
+                    } else {
+                        is_image_icon = "M_board_content_image_block";
+                    }
+                    if (is_attachment_icon == 0) {
+                        is_attachment_icon = "M_board_content_paperclip_none";
+                    } else {
+                        is_attachment_icon = "M_board_content_paperclip_block";
+                    }
+                    if (is_lock_icon == 0){
+                        is_lock_icon = "M_board_content_lock_none";
+                    } else {
+                        is_lock_icon = "M_board_content_lock_block";
+                    }
+                    $(".M_board_contents_container")
+                        .append(
+                            '<div class="' + div_class + '" onclick="postmodal_open('+ post_id +')">' +
+                            '<div class="M_user_profile_color" style="background-color:'+user_color+'"></div>' +
+                            '<div class="M_user_name">' + data.author_name + '</div>' +
+                            '<div class="M_time_info">| ' + submission_time + '</div>' +
+                            '<i class="fas fa-lock M_board_content_icon M_board_content_lock '+is_lock_icon+'"></i>' +
+                            '<i class="fas fa-paperclip M_board_content_icon M_board_content_paperclip '+is_attachment_icon+'"></i>' +
+                            '<i class="fas fa-image M_board_content_icon M_board_content_image '+is_image_icon+'"></i>' +
+                            '<div class="M_board_content_info">' + comment_count + '</div>' +
+                            '<i class="fas fa-comment M_board_content_icon"></i>' +
+                            '<div class="M_board_content_info">' + likes + '</div>' +
+                            '<i class="fas fa-heart M_board_content_icon"></i>' +
+                            '<div class="M_board_content_info">' + views +'</div>' +
+                            '<i class="fas fa-eye M_board_content_icon"></i>' +
+                            '<div class="M_board_content_title">'+ title +'</div>' +
+                            '</div>');
                 }
-                if (is_lock_icon == 0){
-                    is_lock_icon = "M_board_content_lock_none";
-                } else {
-                    is_lock_icon = "M_board_content_lock_block";
-                }
-                $(".M_board_contents_container")
-                    .append(
-                        '<div class="' + div_class + '" onclick="postmodal_open('+ post_id +')">' +
-                        '<div class="M_user_profile_color" style="background-color:'+user_color+'"></div>' +
-                        '<div class="M_user_name">' + data.author_name + '</div>' +
-                        '<div class="M_time_info">| ' + submission_time + '</div>' +
-                        '<i class="fas fa-lock M_board_content_icon M_board_content_lock '+is_lock_icon+'"></i>' +
-                        '<i class="fas fa-paperclip M_board_content_icon M_board_content_paperclip '+is_attachment_icon+'"></i>' +
-                        '<i class="fas fa-image M_board_content_icon M_board_content_image '+is_image_icon+'"></i>' +
-                        '<div class="M_board_content_info">' + comment_count + '</div>' +
-                        '<i class="fas fa-comment M_board_content_icon"></i>' +
-                        '<div class="M_board_content_info">' + likes + '</div>' +
-                        '<i class="fas fa-heart M_board_content_icon"></i>' +
-                        '<div class="M_board_content_info">' + views +'</div>' +
-                        '<i class="fas fa-eye M_board_content_icon"></i>' +
-                        '<div class="M_board_content_title">'+ title +'</div>' +
-                        '</div>');
             }
         });
     }
 );
 
 let page = 2;
-let flag = true;
 $(window).scroll(function() {
-    if ($(window).scrollTop() >= $(document).height() - $(window).height()) {
-        if (flag === true)
+    if ($(window).scrollTop() + 1 >= $(document).height() - $(window).height()) {
+        if (flag === false)
         {
+            flag = true;
             let a_jax = A_JAX(TEST_IP+'get_posts/공지/'+page, 'GET', null, null);
             $.when(a_jax).done(function () {
-                let div_class = 'M_info_div M_board_content M_boxshadow wow flipInX';
-                if (localStorage.getItem('modakbul_theme') === 'dark') {
-                    div_class +=  ' M_boxshadow_dark_shadow" style="' +
-                        'visibility: visible; background-color: rgb(73, 78, 82); color: rgb(245, 246, 250); border: 0px solid rgb(221, 221, 221);';
-                }
-                if (a_jax.responseJSON.posts.length > 1)
-                {
-                    for (let i=0; i<a_jax.responseJSON.posts.length; i++) {
-                        let data = a_jax.responseJSON.posts[i];
-                        //let submission_time = format_time(data.post_date);
-                        let submission_time = data.post_date;
-                        let title = data.post_title;
-                        let views = data.post_view;
-                        let comment_count = data.comment_cnt;
-                        let likes = data.like_cnt;
-                        let post_id = data.post_id;
-                        let user_color = data.author_color;
-                        let is_image_icon = data.img_cnt;
-                        let is_attachment_icon = data.file_cnt;
-                        let is_lock_icon = data.private;
-                        if (is_image_icon == 0){
-                            is_image_icon = "M_board_content_image_none";
-                        } else {
-                            is_image_icon = "M_board_content_image_block";
-                        }
-                        if (is_attachment_icon == 0) {
-                            is_attachment_icon = "M_board_content_paperclip_none";
-                        } else {
-                            is_attachment_icon = "M_board_content_paperclip_block";
-                        }
-                        if (is_lock_icon == 0){
-                            is_lock_icon = "M_board_content_lock_none";
-                        } else {
-                            is_lock_icon = "M_board_content_lock_block";
-                        }
-
-                        $(".M_board_contents_container")
-                            .append(
-                                '<div class="' + div_class + '" onclick="postmodal_open('+ post_id +')">' +
-                                '<div class="M_user_profile_color" style="background-color:'+user_color+'"></div>' +
-                                '<div class="M_user_name">' + data.author_name + '</div>' +
-                                '<div class="M_time_info">| ' + submission_time + '</div>' +
-                                '<i class="fas fa-lock M_board_content_icon M_board_content_lock '+is_lock_icon+'"></i>' +
-                                '<i class="fas fa-paperclip M_board_content_icon M_board_content_paperclip '+is_attachment_icon+'"></i>' +
-                                '<i class="fas fa-image M_board_content_icon M_board_content_image '+is_image_icon+'"></i>' +
-                                '<div class="M_board_content_info">' + comment_count + '</div>' +
-                                '<i class="fas fa-comment M_board_content_icon"></i>' +
-                                '<div class="M_board_content_info">' + likes + '</div>' +
-                                '<i class="fas fa-heart M_board_content_icon"></i>' +
-                                '<div class="M_board_content_info">' + views +'</div>' +
-                                '<i class="fas fa-eye M_board_content_icon"></i>' +
-                                '<div class="M_board_content_title">'+ title +'</div>' +
-                                '</div>');
+                if (a_jax.responseJSON['result'] == 'success'){
+                    let div_class = 'M_info_div M_board_content M_boxshadow wow flipInX';
+                    if (localStorage.getItem('modakbul_theme') === 'dark') {
+                        div_class +=  ' M_boxshadow_dark_shadow" style="' +
+                            'visibility: visible; background-color: rgb(73, 78, 82); color: rgb(245, 246, 250); border: 0px solid rgb(221, 221, 221);';
                     }
-                    page++;
+                    if (a_jax.responseJSON.posts.length > 1)
+                    {
+                        for (let i=0; i<a_jax.responseJSON.posts.length; i++) {
+                            let data = a_jax.responseJSON.posts[i];
+                            let submission_time = data.post_date;
+                            let title = data.post_title;
+                            let views = data.post_view;
+                            let comment_count = data.comment_cnt;
+                            let likes = data.like_cnt;
+                            let post_id = data.post_id;
+                            let user_color = data.author_color;
+                            let is_image_icon = data.img_cnt;
+                            let is_attachment_icon = data.file_cnt;
+                            let is_lock_icon = data.private;
+                            if (is_image_icon == 0){
+                                is_image_icon = "M_board_content_image_none";
+                            } else {
+                                is_image_icon = "M_board_content_image_block";
+                            }
+                            if (is_attachment_icon == 0) {
+                                is_attachment_icon = "M_board_content_paperclip_none";
+                            } else {
+                                is_attachment_icon = "M_board_content_paperclip_block";
+                            }
+                            if (is_lock_icon == 0){
+                                is_lock_icon = "M_board_content_lock_none";
+                            } else {
+                                is_lock_icon = "M_board_content_lock_block";
+                            }
+                            $(".M_board_contents_container")
+                                .append(
+                                    '<div class="' + div_class + '" onclick="postmodal_open('+ post_id +')">' +
+                                    '<div class="M_user_profile_color" style="background-color:'+user_color+'"></div>' +
+                                    '<div class="M_user_name">' + data.author_name + '</div>' +
+                                    '<div class="M_time_info">| ' + submission_time + '</div>' +
+                                    '<i class="fas fa-lock M_board_content_icon M_board_content_lock '+is_lock_icon+'"></i>' +
+                                    '<i class="fas fa-paperclip M_board_content_icon M_board_content_paperclip '+is_attachment_icon+'"></i>' +
+                                    '<i class="fas fa-image M_board_content_icon M_board_content_image '+is_image_icon+'"></i>' +
+                                    '<div class="M_board_content_info">' + comment_count + '</div>' +
+                                    '<i class="fas fa-comment M_board_content_icon"></i>' +
+                                    '<div class="M_board_content_info">' + likes + '</div>' +
+                                    '<i class="fas fa-heart M_board_content_icon"></i>' +
+                                    '<div class="M_board_content_info">' + views +'</div>' +
+                                    '<i class="fas fa-eye M_board_content_icon"></i>' +
+                                    '<div class="M_board_content_title">'+ title +'</div>' +
+                                    '</div>');
+                        }
+                        page++;
+                    }
                 }
-                else
-                {
-                    page++;
-                    flag = false;
-                }
+                flag = false;
             });
         }
     }
