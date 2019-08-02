93e512129f535888c5f19ca91a4f808c6b058a9a
James Jung <jamesj0107@naver.com>
2019-08-03 00:32:30 +0900

    Shit

--- src/static/js/drag_drop.js
+++ src/static/js/drag_drop.js
@@ -21,39 +21,31 @@ $.when(ajax).done(function () {
 
     for (let i=0; i<response_data.length; i++)
     {
-        let board = response_data[i];
-        if (board.board_url.indexOf('_') === -1)
-        {
-            $('#M_sortable_list').append(
-                '<div data-id="'+board.board_url+'"class="M_sortable M_sortable_menu">'+board.board_name+'</div>');
-        }
-        else
-        {
-            if (board.board_url.split('_')[1] === '') {
-                let board_html = '<div id="M_sortable_'+ i +'" data-id="'+board.board_url+
-                    '"class="M_sortable M_sortable_menu">' + board.board_name;
-                for (let j=0; j<response_data.length; j++) {
-                    if (response_data[j].board_url.indexOf(board.board_url) === 0 && response_data[j] !== board) {
-                        board_html += '<div data-id="'+response_data[j].board_url+
-                            '"class="M_sortable M_sortable_menu">'+response_data[j].board_name+'</div>';
-                    }
-                }
-                board_html += '</div>';
-                $('#M_sortable_list').append(board_html);
-
-                let board_list = document.getElementById('M_sortable_' + i);
-                let new_sortable = new Sortable(board_list, {
-                    group: {
-                        name: 'M_sortable_' + i,
-                        put: false
-                    },
-                    animation: 150,
-                    fallbackOnBody: true,
-                    swapThreshold: 0.65
-                });
-                sortable_list.push(new_sortable);
+    let board = response_data[i];
+    if (board.board_url.split('_')[1] === '') {
+        let board_html = '<div id="M_sortable_'+ i +'" data-id="'+board.board_url+
+            '"class="M_sortable M_sortable_menu">' + board.board_name;
+        for (let j=0; j<response_data.length; j++) {
+            if (response_data[j].board_url.indexOf(board.board_url) === 0 && response_data[j] !== board) {
+                board_html += '<div data-id="'+response_data[j].board_url+
+                    '"class="M_sortable M_sortable_menu">'+response_data[j].board_name+'</div>';
             }
         }
+        board_html += '</div>';
+        $('#M_sortable_list').append(board_html);
+
+        let board_list = document.getElementById('M_sortable_' + i);
+        let new_sortable = new Sortable(board_list, {
+            group: {
+                name: 'M_sortable_' + i,
+                put: false
+            },
+            animation: 150,
+            fallbackOnBody: true,
+            swapThreshold: 0.65
+        });
+        sortable_list.push(new_sortable);
+    }
     }
     for (let i=0; i<sortable_list.length; i++){
         console.log(sortable_list[i].toArray());
