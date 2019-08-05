222f4d0b20d084c04e3202dd01a05788664f8d23
James <jamesj0107@naver.com>
2019-08-06 03:35:41 +0900

    Create user search on settings

--- src/static/js/setting.js
+++ src/static/js/setting.js
@@ -1,3 +1,4 @@
+const DEPARTMENTS = ['데이터사이언스학과', '디자인이노베이션', '디지털콘텐츠학과', '만화애니메이션텍', '소프트웨어학과', '정보보호학과', '지능기전공학부', '창의소프트학부', '컴퓨터공학과'];
 $(document).ready(()=>{
    let ajax = A_JAX(TEST_IP+'get_variables', "GET", null, null);
    $.when(ajax).done(()=>{
@@ -16,8 +17,35 @@ $(document).ready(()=>{
        $('#M_image_preview').attr('src', '../static/image/'+image[0].value);
    });
 
-
+   black_list();
 });
+
+function black_list() {
+    let black_ajax = A_JAX(TEST_IP+'get_blacklist', 'GET', null, null);
+    $.when(black_ajax).done(()=>{
+        result_html = ''
+        for (let i=0; i<black_ajax.responseJSON.blacklist.length; i++) {
+            let major = '';
+            for (let j=0; j<DEPARTMENTS.length; j++)
+            {
+                if (black_ajax.responseJSON.blacklist[i].tags.indexOf(DEPARTMENTS[j]) !== -1) {
+                    major = DEPARTMENTS[j];
+                }
+            }
+
+
+            result_html += '<div class="M_black_user_info">' +
+                '<div class="M_setting_user_tag"></div>'+
+                '<div class="M_setting_subtitle">'+
+                black_ajax.responseJSON.blacklist[i].user_name + ' ' +
+                black_ajax.responseJSON.blacklist[i].user_id + ' ' +
+                major +
+                '</div>'+
+                '<div onclick="white_user($(this))" class="M_setting_unblack_button">취소</div></div>';
+        }
+        $('.M_setting_blacklist').append(result_html);
+    })
+}
 
 function toggle(flag) {
     let info = $('.M_info');
@@ -156,4 +184,65 @@ function settingsPage_check_admin() {
         alert("접근 권한이 없습니다.");
         location.href = "/";
     });
+}
+function search_user() {
+    let search = $('.M_user_search').val();
+    let ajax = A_JAX(TEST_IP+"get_user_search", "POST", null, {'search': search});
+    let result_html = '';
+    $.when(ajax).done(()=>{
+        if (ajax.responseJSON.result === 'user is not defined') {
+            snackbar('사용자가 없습니다.')
+        }
+        else {
+            let major = '';
+            for (let j=0; j<DEPARTMENTS.length; j++)
+            {
+                if (ajax.responseJSON.user_tags.indexOf(DEPARTMENTS[j]) !== -1) {
+                    major = DEPARTMENTS[j];
+                }
+            }
+
+            result_html +=
+                '<div class="M_setting_user_tag"></div>'+
+                '<div class="M_setting_subtitle">'+
+                ' ' + ajax.responseJSON.user.user_name + ' ' + ajax.responseJSON.user.user_id + ' '+ major +
+                '</div>'+
+                '<div onclick="black_user($(this).parent())" class = "M_setting_black_button"> 블랙</div>';
+            $('#M_user_info').empty();
+            $('#M_user_info').append(result_html);
+        }
+    })
+}
+
+$('.M_user_search').keypress(function (e) {
+    let key = e.which;
+    if(key === 13)
+    {
+        search_user();
+    }
+});
+
+function black_user(div) {
+    let ajax = A_JAX(TEST_IP+"user_black_apply", "POST", null, {'target_id': div[0].childNodes[1].innerText.split(' ')[1]});
+    $.when(ajax).done(()=>{
+        if (ajax.responseJSON.result === 'already blacklist') {
+            snackbar('이미 블랙리스트에 추가된 사용자 입니다.');
+            $('#M_user_info').empty();
+        }
+        else {
+            $('#M_user_info').empty();
+            $('.M_setting_blacklist').empty();
+            black_list();
+        }
+    })
+}
+function white_user(div) {
+    let ajax = A_JAX(TEST_IP+"user_black_cancel", "POST", null, {'target_id': div.parent()[0].childNodes[1].innerText.split(' ')[1]});
+    $.when(ajax).done(()=>{
+        if (ajax.responseJSON.result === 'no blacklist') {
+            snackbar('이미 블랙리스트에서 제거된 사용자 입니다.');
+        }
+        $('.M_setting_blacklist').empty();
+        black_list();
+    })
 }
